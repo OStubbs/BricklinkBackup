798097ac83b9ec670b3692539ee3e8249b5e6c01
OStubbs <ollyrstubbs@gmail.com>
2023-11-09 22:51:20 +0000

    Attempt to create a brickstore xml

--- bricklink_backup.py
+++ bricklink_backup.py
@@ -4,12 +4,12 @@ import re
 import sys
 import toml
 import argparse
-from json2xml import json2xml
+import textwrap
 from pathlib import Path
 from datetime import datetime
 from requests_oauthlib import OAuth1Session
 
-class FileWriter:
+class StringManipulation:
     @staticmethod
     def slugify(value, allow_unicode=False):
         """
@@ -29,8 +29,21 @@ class FileWriter:
             )
         value = re.sub(r"[^\w\s-]", "", value.lower())
         return re.sub(r"[-\s]+", "-", value).strip("-_")
-
-
+    
+    
+    @staticmethod
+    def escape_xml(str_xml):
+        ''' Random function I found that I am too lazy to change. '''
+        if type(str_xml) != str:
+            return str_xml
+        str_xml = str_xml.replace("&", "&amp;")
+        str_xml = str_xml.replace("<", "&lt;")
+        str_xml = str_xml.replace(">", "&gt;")
+        str_xml = str_xml.replace("\"", "&quot;")
+        str_xml = str_xml.replace("'", "&apos;")
+        return str_xml
+    
+class FileWriter:
     @staticmethod
     def flatten_json(y):
         """Recursively flattens a nested JSON."""
@@ -48,7 +61,7 @@ class FileWriter:
 
         flatten(y)
         return out
-
+    
     @staticmethod
     def json_to_csv(data, output_filename, path):
         """Takes a JSON (with possible nested dictionaries) and exports to a single CSV file."""
@@ -68,19 +81,56 @@ class FileWriter:
             dict_writer.writerows(flat_data)
 
     @staticmethod
-    def json_to_xml(data, output_filename, path):
+    def json_to_xml(data, output_filename, path, categories):
         """Takes a JSON (with possible nested dictionaries) and exports to a single CSV file."""
-        print("Writing data into .xml file....")
+        print("Writing data into a BrickStore .xml file....")
         # As it's a changeable path, check and make directory if doesn't exist
         Path(path).mkdir(parents=True, exist_ok=True)
 
-        output_filename = FileWriter.slugify(output_filename)
-        #print(data)
+        output_filename = StringManipulation.slugify(output_filename)
 
-        j2xml = json2xml.Json2xml(data, wrapper="sequence", pretty=True, attr_type=False).to_xml()
-        j2xml.encode('utf-8')
+        items_xml = ""
+        for item in data:
+            for key in item.keys():
+                item_type = type(item[key])
+                if item_type == dict:
+                    for sub_key in item[key]:
+                        item[key][sub_key] = StringManipulation.escape_xml(item[key][sub_key])
+                else:
+                    item[key] = StringManipulation.escape_xml(item[key])
+
+            items_xml += f"""
+                <ITEM>
+                    <ITEMID>{item["item"]['no']}</ITEMID>
+                    <ITEMTYPEID>{item["item"]['type'][0]}</ITEMTYPEID>
+                    <COLORID>{item["color_id"]}</COLORID>
+                    <ITEMNAME>{item["item"]['name']}</ITEMNAME>
+                    <ITEMTYPENAME>{item["item"]['type']}</ITEMTYPENAME>
+                    <COLORNAME>{item['color_name']}</COLORNAME>
+                    <CATEGORYID>{item["item"]['category_id']}</CATEGORYID>
+                    <CATEGORYNAME>{item["item"]['category_name']}</CATEGORYNAME>
+                    <STATUS>I</STATUS>
+                    <BULK>{item['bulk']}</BULK>
+                    <COST>{item['my_cost']}</COST>
+                    <QTY>{item['quantity']}</QTY>
+                    <PRICE>{item['unit_price']}</PRICE>
+                    <CONDITION>{item['new_or_used']}</CONDITION>
+                </ITEM>
+                """
+        inventory = f"""\
+        <INVENTORY>
+            {items_xml}
+        </INVENTORY>
+        """
+        xml = textwrap.dedent(f"""\
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE BrickStoreXML>
+        {inventory}
+        """)
+
         with open(f"{path}/{output_filename}.xml", 'w', encoding='utf-8', errors='replace') as f:
-            f.write(j2xml)
+            f.write(xml)
+            
 
 class Config:
     def __init__(self, path):
@@ -92,6 +142,9 @@ class Bricklink:
         self.config = config.data
         self.setup_keys()
         self.auth_session()
+
+        self.inventories = []
+        self.categories = []
 
     def setup_keys(self):
         """Setting up variables from config"""
@@ -116,10 +169,9 @@ class Bricklink:
             resource_owner_key=self.TOKEN_VALUE,
             resource_owner_secret=self.TOKEN_SECRET
         )
-
-    def fetch_inventory(self):
-        print("Getting data from the Bricklink API...")
-        response = self.oauth.get(f'https://api.bricklink.com/api/store/v1/inventories')
+    
+    def _api_call(self, end_point):
+        response = self.oauth.get(f'https://api.bricklink.com/api/store/v1/{end_point}')
 
         if response.status_code != 200:
             print(f"Error: {response.text}")
@@ -127,15 +179,28 @@ class Bricklink:
         response.encoding = 'utf-8'
         
         return response.json()['data']
+    
+    def _set_category_names(self):
+        for inv_index, inv_item in enumerate(self.inventories):
+            inv_item["item"]["category_name"] = ""
+            for i, dic in enumerate(self.categories):
+                if dic["category_id"] == inv_item["item"]["category_id"]:
+                    self.inventories[inv_index]["item"]["category_name"] = self.categories[i]["category_name"]
+
+    
+    def fetch_inventory(self):
+        self.inventories = self._api_call("inventories")
+        self.categories = self._api_call("categories")
 
     def save_all_inventories(self, file_type):
-        data = self.fetch_inventory()
+        self.fetch_inventory()
+        self._set_category_names()
         filename = f"{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')} - Bricklink Backup"
-
+        path = self.config['EXPORT']['PATH']
         if file_type.lower() == "csv":
-            FileWriter.json_to_csv(data, filename, self.config['EXPORT']['PATH'])
+            FileWriter.json_to_csv(self.inventories, filename, path)
         else:
-            FileWriter.json_to_xml(data, filename, self.config['EXPORT']['PATH'])
+            FileWriter.json_to_xml(self.inventories, filename, path, self.categories)
         print('#' * 25)
         print("File saved!")
 
